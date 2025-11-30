be6007d7b727171eaae36e2af4b18dccf3d52b8d
Nur Kholish <kholish.nur8306@gmail.com>
2025-11-30 15:32:14 +0700

    cleaning resources

--- FishMart/Properties/Resources.Designer.cs
+++ FishMart/Properties/Resources.Designer.cs
@@ -253,16 +253,6 @@ namespace FishMart.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap btnToggle {
-            get {
-                object obj = ResourceManager.GetObject("btnToggle", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap BuatAkun {
             get {
                 object obj = ResourceManager.GetObject("BuatAkun", resourceCulture);
